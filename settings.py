5c83062b2ed606a1dccbf9470c4e6078a42dc5fd
Henrique Lopes <riquellopes@gmail.com>
2014-04-19 08:04:32 -0300

    atualizando settings

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from os import path, environ
 
+SECRET_KEY=environ.get('SECRET_KEY')
 CONSUMER_SECRET=environ.get('CONSUMER_SECRET')
 CONSUMER_KEY=environ.get('CONSUMER_KEY')
 TEST=environ.get('TEST')
