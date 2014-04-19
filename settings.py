b1c451ae65480e498452e25206bbe285c44ec071
Henrique Lopes <riquellopes@gmail.com>
2014-04-19 07:57:08 -0300

    atualizando settings

--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from os import path, environ
 
+CONSUMER_SECRET=environ.get('CONSUMER_SECRET')
+CONSUMER_KEY=environ.get('CONSUMER_KEY')
 TEST=environ.get('TEST')
 DEBUG=environ.get('DEBUG')
 FACEBOOK_CONSUMER_KEY=environ.get('FACEBOOK_CONSUMER_KEY')
